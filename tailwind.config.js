083924eb25a3b2effbd4436b8ece4e3ddc6309a2
Rani Bungay <ranibungay@gmail.com>
2025-01-06 14:52:23 -0800

    change button color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
         textColor: "#042C23", // main text color
         lightTextColor: "#ACCBe1", // text on dark backgrounds
         logoColor: "#ACCBE1", // main homi logo
-        buttonColor: "#ACCBE1",
+        buttonColor: "#7C98B3",
         cardColor: "#D9D9D9", // background for card
         whiteColor: "#FFFFFF", // white for forms, backgrounds for cards
         backgroundColor: "#7C98B3",
@@ -25,17 +25,3 @@ export default {
   },
   plugins: [],
 };
-
-/*
-
-textColor: "#042C23", // main text color
-lightTextColor: "#E1EBE0", // text on dark backgrounds
-logoColor: "#65B6A3", // main homi logo
-buttonColor: "#65B6A3", // button teal
-cardColor: "#D9D9D9", // background for card
-whiteColor: "#FFFFFF", // white for forms, backgrounds for cards
-backgroundColor: "#E1EBE0", // honeydew for background
-alternativeColor: "#417367", // background for dark cards
-darkColor: "#042C23" // for footer
-
-*/
