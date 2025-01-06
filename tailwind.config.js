f79fea9f76116ea8856397e600a1708075510d8d
Rani Bungay <ranibungay@gmail.com>
2025-01-06 14:46:53 -0800

    change mapbox key

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,23 +5,37 @@ export default {
     extend: {
       colors: {
         textColor: "#042C23", // main text color
-        lightTextColor: "#E1EBE0", // text on dark backgrounds
-        logoColor: "#65B6A3", // main homi logo
-        buttonColor: "#65B6A3", // button teal
+        lightTextColor: "#ACCBe1", // text on dark backgrounds
+        logoColor: "#ACCBE1", // main homi logo
+        buttonColor: "#ACCBE1",
         cardColor: "#D9D9D9", // background for card
         whiteColor: "#FFFFFF", // white for forms, backgrounds for cards
-        backgroundColor: "#E1EBE0", // honeydew for background
-        alternativeColor: "#417367", // background for dark cards
-        darkColor: "#042C23" // for footer
+        backgroundColor: "#7C98B3",
+        alternativeColor: "#637081", // background for dark cards
+        darkColor: "#536B78", // for footer
       },
       fontFamily: {
         sans: ["Quicksand", "sans-serif"],
-        serif: ["Montserrat", "serif"]
+        serif: ["Montserrat", "serif"],
       },
       maxHeight: {
-        'amenities': '20rem',
-      }
+        amenities: "20rem",
+      },
     },
   },
   plugins: [],
 };
+
+/*
+
+textColor: "#042C23", // main text color
+lightTextColor: "#E1EBE0", // text on dark backgrounds
+logoColor: "#65B6A3", // main homi logo
+buttonColor: "#65B6A3", // button teal
+cardColor: "#D9D9D9", // background for card
+whiteColor: "#FFFFFF", // white for forms, backgrounds for cards
+backgroundColor: "#E1EBE0", // honeydew for background
+alternativeColor: "#417367", // background for dark cards
+darkColor: "#042C23" // for footer
+
+*/
